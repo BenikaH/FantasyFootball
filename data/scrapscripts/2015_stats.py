bc5bdf45187a8ac5d0983f2b947f4b476db3edf5
Neeraj Asthana <nasthan2@illinois.edu>
2016-03-21 21:36:38 -0400

    code to get team and position but problems when there is an injury

--- data/scrapscripts/2015_stats.py
+++ data/scrapscripts/2015_stats.py
@@ -15,4 +15,25 @@ baseurl = "http://games.espn.go.com/ffl/leaders?&startIndex="
 headers = ["Name", "Position", "Rank", "RUSH", "RUSHYDS", "RUSHTD", "TAR", "REC", "RECYDS", "RECTD", "C", "A", "COMPYDS", "COMPTD", "INT"] + ["PTS", "TEAM"]
 
 for index in range(start_index, num_players, index_increment):
-	
+    #Set the url and read in the raw html
+    url = baseurl + str(index)
+    html = urlopen(url).read()
+    soup = BeautifulSoup(html, "html.parser")
+
+    #extract all player rows
+    table_enteries = soup.findAll("tr", "pncPlayerRow")
+
+    for tab in table_enteries:
+        #extract the name of the player
+        name = tab.find("a", "flexpop").string
+
+        #extract team name and position
+        line = str(tab.find("td", "playertablePlayerName"))
+        por = line[line.find("</a>")+6:-5]
+        position = por[por.find("\xa0")+1:]
+        if position == "D/ST":
+            team = "--"
+        else:
+            team = por[:por.find("\xa0")-1]
+
+        print(name, team, position)
