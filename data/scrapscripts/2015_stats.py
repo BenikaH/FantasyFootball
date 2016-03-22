09b5f4704a5ff25d737193339364a30771c62b0f
Neeraj Asthana <nasthan2@illinois.edu>
2016-03-22 19:50:13 -0400

    gathered all data for 2015 statistics

--- data/scrapscripts/2015_stats.py
+++ data/scrapscripts/2015_stats.py
@@ -1,4 +1,4 @@
-###Script to get ESPN 2015 stats for top 350 players
+###Script to get ESPN 2015 stats for top 400 players
 #http://games.espn.go.com/ffl/leaders?&startIndex=0
 
 #libraries
@@ -7,21 +7,22 @@ from urllib2 import urlopen
 import csv
 
 #parameters
-num_players = 50
+num_players = 400
 index_increment = 50
 start_index = 0
 baseurl = "http://games.espn.go.com/ffl/leaders?&startIndex="
 
 headers = ["Name", "Position", "Rank", "RUSH", "RUSHYDS", "RUSHTD", "TAR", "REC", "RECYDS", "RECTD", "C", "A", "COMPYDS", "COMPTD", "INT", "TWOPC", "FUM", "MISCTD"] + ["PTS", "TEAM"]
 
+#data structure to hold all of the players that we have parsed
+players = []
+rank = 1 #keep track of how well player did
+
 for index in range(start_index, num_players, index_increment):
     #Set the url and read in the raw html
     url = baseurl + str(index)
     html = urlopen(url).read()
     soup = BeautifulSoup(html, "html.parser")
-
-    #rank count
-    rank = 1
 
     #extract all player rows
     table_enteries = soup.findAll("tr", "pncPlayerRow")
@@ -71,5 +72,17 @@ for index in range(start_index, num_players, index_increment):
         #add points and rank
         player2015["PTS"] = int(tab.find("td", "playertableStat appliedPoints sortedCell").string)
         player2015["Rank"] = rank
+        rank += 1
 
-        print(player2015)
+        players.append(player2015)
+
+    #progress of script
+    print(rank)
+
+
+#write csv file
+with open('../formatted_data/2015stats.csv', 'w') as csvfile:    
+    writer = csv.DictWriter(csvfile, fieldnames=headers, delimiter=',', quoting=csv.QUOTE_NONNUMERIC)
+    writer.writeheader()
+    for piece in players:
+        writer.writerow(piece)
