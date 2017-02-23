3edc9ef6ce748188b3775963e6a79348f7c1f857
Neeraj Asthana <nasthan2@illinois.edu>
2017-02-22 23:27:55 -0500

    Closes [neeasthana/FantasyFootball#14]. Cleaned pro football reference data with naming issue

--- data/scripts/data_cleaning/clean_profootball_names.py
+++ data/scripts/data_cleaning/clean_profootball_names.py
@@ -1,45 +1,20 @@
 '''
-Takes standard_fantasy_2014.csv  standard_fantasy_2015.csv  standard_fantasy_2016.csv and transforms them from standard scoring to ppr scoring
-Takes full file name (starting from ~/...) as first command line argument
+Takes names from data collection on profootball-reference and changes name to be of the format that is more friendly
+Ex. Ezekiel Elliott*+\ElliEz00 -> Ezekiel Elliott
 '''
-import csv
 import sys
+import pandas as pd
 
 if len(sys.argv) < 2:
     raise ("must have a file name! add file name as first command line argument")
-else:
-    file_loc = sys.argv[1]
 
-    results = []
+file_loc = sys.argv[1]
 
-    with open(file_loc) as csvfile:
-        reader = csv.DictReader(csvfile)
-        for row in reader:
-            #Calculate ppr points value
+df = pd.read_csv(file_loc)
 
-            ppr_player_pts = 0
-            if row['FantPt'] == "" and row['Rec'] == "":
-                ppr_player_pts = 0
-            elif row['FantPt'] == "":
-                ppr_player_pts = round(float(row['Rec']), 1)
-            elif row['Rec'] == "":
-                ppr_player_pts = round(float(row['FantPt']), 1)
-            else:
-                ppr_player_pts = round(float(row['FantPt']) + float(row['Rec']), 1)
+names = df.ix[:,"Name"]
+new_names = names.map(lambda x: x[:-9].replace("*","").replace("+",""))
 
-            new_player = row
-            new_player['FantPprPt'] = ppr_player_pts
-            new_player.pop('FantPt')
-            results.append(new_player)
+df["Name"] = new_names
 
-    print(results[1].keys())
-
-    #write file
-    write_file = "ppr" + file_loc[file_loc.rfind("/")+9:]
-    with open(write_file, 'w') as csvfile:
-        fieldnames = results[1].keys()
-        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
-
-        writer.writeheader()
-        for row in results:
-            writer.writerow(row)
+df.to_csv(file_loc)
