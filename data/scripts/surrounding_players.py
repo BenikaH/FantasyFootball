2c6803f6a58d381a4a500bef31526cd0cb1eeb9c
Neeraj Asthana <nasthan2@illinois.edu>
2017-04-06 22:48:17 -0400

    merged datasets together

--- data/scripts/surrounding_players.py
+++ data/scripts/surrounding_players.py
@@ -2,15 +2,15 @@
 Generates data for surrouding players for specific positions
 
 Inputs (sys.args):
-	[1] team_stats_file - file location of team statstics
-	[2] actual_file - file location of fantasy performance for all players for a specific years
+    [1] team_stats_file - file location of team statstics
+    [2] actual_file - file location of fantasy performance for all players for a specific years
 Outputs:
-	csv file with postional data for surrounding players
+    csv file with postional data for surrounding players
 '''
 import sys
 import pandas as pd
 
-# Step 1 - read command line arguments and open csvs
+# Step 1 - read command line arguments and open csvs and clean data
 
 team_stats_file = sys.argv[1]
 actual_file = sys.argv[2]
@@ -18,40 +18,51 @@ actual_file = sys.argv[2]
 team_stats = pd.read_csv(sys.argv[1])
 actual = pd.read_csv(sys.argv[2])
 
-# Step  - Match teams in team stats vs. actual player season stats
+if 'G' in list(team_stats):
+    del team_stats['G']
+
+# Step 2 - Match teams in team stats vs. actual player season stats.
+#          Change team statistics column for "Tm" to actual player dataset's representation
 team_dictionary = {
-	"Atlanta Falcons": "ATL",
-	"New Orleans Saints": "NOR",
-	"New Englad Patriots": "NWE",
-	"Green Bay Packers": "GNB",
-	"Dallas Cowboys": "DAL",
-	"Arizona Cardinals": "ARI",
-	"Oakland Raiders": "OAK",
-	"Indianapolis Colts": "IND",
-	"San Diego Chargers": "SDG",
-	"Pittsburgh Steelers": "PIT",
-	"Buffalo Bills": "BUF",
-	"Washington Redskins": "WAS",
-	"Kansas City Chiefs": "KAN",
-	"Tennessee Titans": "TEN",
-	"Carolina Panthers": "CAR",
-	"Philadelphia Eagles": "PHI",
-	"Miami Dolphins": "MIA",
-	"Seattle Seahawks": "SEA",
-	"Tampa Bay Buccaneers": "TAM",
-	"Detroit Lions": "DET",
-	"Baltimore Ravens": "BAL",
-	"Denver Broncos": "DEN",
-	"Minnesota Vikings": "MIN",
-	"Cincinnati Bengals": "CIN",
-	"Jacksonville Jaguars": "JAX",
-	"New York Giants": "NYG",
-	"San Francisco 49ers": "SFO",
-	"Chicago Bears": "CHI",
-	"Houston Texans": "HOU",
-	"New York Jets": "NYJ",
-	"Cleveland Browns": "CLE",
-	"Los Angeles Rams": "LAR"
+    "Atlanta Falcons": "ATL",
+    "New Orleans Saints": "NOR",
+    "New England Patriots": "NWE",
+    "Green Bay Packers": "GNB",
+    "Dallas Cowboys": "DAL",
+    "Arizona Cardinals": "ARI",
+    "Oakland Raiders": "OAK",
+    "Indianapolis Colts": "IND",
+    "San Diego Chargers": "SDG",
+    "Pittsburgh Steelers": "PIT",
+    "Buffalo Bills": "BUF",
+    "Washington Redskins": "WAS",
+    "Kansas City Chiefs": "KAN",
+    "Tennessee Titans": "TEN",
+    "Carolina Panthers": "CAR",
+    "Philadelphia Eagles": "PHI",
+    "Miami Dolphins": "MIA",
+    "Seattle Seahawks": "SEA",
+    "Tampa Bay Buccaneers": "TAM",
+    "Detroit Lions": "DET",
+    "Baltimore Ravens": "BAL",
+    "Denver Broncos": "DEN",
+    "Minnesota Vikings": "MIN",
+    "Cincinnati Bengals": "CIN",
+    "Jacksonville Jaguars": "JAX",
+    "New York Giants": "NYG",
+    "San Francisco 49ers": "SFO",
+    "Chicago Bears": "CHI",
+    "Houston Texans": "HOU",
+    "New York Jets": "NYJ",
+    "Cleveland Browns": "CLE",
+    "Los Angeles Rams": "LAR"
 }
 
-print(team_dictionary)
+for index, row in team_stats.iterrows():
+    team_stats.ix[index,"Tm"] = team_dictionary[row["Tm"]]
+
+
+# Step 3 - Match players with team stats
+df = pd.merge(actual, team_stats,on="Tm", suffixes=["","Tm"])
+
+# Step 4 - Add columns with player stats subtracted from team totals (accurate team contribution)
