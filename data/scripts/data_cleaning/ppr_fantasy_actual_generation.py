2ca2e34718863c1084ef4aff04bdaffabf178058
Neeraj Asthana <nasthan2@illinois.edu>
2017-05-25 22:44:58 -0400

    Reference [neeasthana/FantasyFootball#52]. Fixes issue with ppr issue and recreates ppr actual scoring files

--- data/scripts/data_cleaning/ppr_fantasy_actual_generation.py
+++ data/scripts/data_cleaning/ppr_fantasy_actual_generation.py
@@ -14,40 +14,8 @@ df =  pd.read_csv(file_loc)
 df['FantPprPt'] = df["FantPt"].fillna(0) + df["Rec"].fillna(0)
 df['FantPprPtpg'] = round((df["FantPt"].fillna(0) + df["Rec"].fillna(0))/df["G"],2)
 
-output_file_name = "combined_" + file_loc.split("/")[-1]
+del df["FantPt"]
+del df["FantPtpg"]
+
+output_file_name = "ppr_" + file_loc.split("/")[-1]
 df.to_csv(output_file_name)
-"""
-df.drop('reports', axis=1)
-
-    with open(file_loc) as csvfile:
-        reader = csv.DictReader(csvfile)
-        for row in reader:
-            #Calculate ppr points value
-
-            ppr_player_pts = 0
-            if row['FantPt'] == "" and row['Rec'] == "":
-                ppr_player_pts = 0
-            elif row['FantPt'] == "":
-                ppr_player_pts = round(float(row['Rec']), 1)
-            elif row['Rec'] == "":
-                ppr_player_pts = round(float(row['FantPt']), 1)
-            else:
-                ppr_player_pts = round(float(row['FantPt']) + float(row['Rec']), 1)
-
-            new_player = row
-            new_player['FantPprPt'] = ppr_player_pts
-            new_player.pop('FantPt')
-            results.append(new_player)
-
-    print(results[1].keys())
-
-    #write file
-    write_file = "ppr" + file_loc[file_loc.rfind("/")+9:]
-    with open(write_file, 'w') as csvfile:
-        fieldnames = results[1].keys()
-        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
-
-        writer.writeheader()
-        for row in results:
-            writer.writerow(row)
-"""
